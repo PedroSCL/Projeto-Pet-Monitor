63d7ef0814bc9439e1431dbe32e9226d54b0dee6
Gustavo-CS <gustavocarvalho9340@gmail.com>
2023-06-28 17:13:21 -0300

    min-vh outros desconsiderar

--- pet-cadastro/js/bd.js
+++ pet-cadastro/js/bd.js
@@ -9,6 +9,18 @@ loginForm.addEventListener("submit", evento => {
     //console.log(data);
     const url = 'https://projeto-2-teste-c9255-default-rtdb.firebaseio.com/.json';
 
+    
+
+    let email = document.getElementById("email").value;
+    console.log(email);
+    let cpf = document.getElementById("cpf").value;
+    console.log(cpf);
+    let tel = document.getElementById("tel").value;
+    console.log(tel);
+    let endereco = document.getElementById("endereco").value;
+    console.log(endereco);
+    let nome = document.getElementById("nome").value;
+    console.log(nome);
 
     let idade = document.getElementById("idade").value;
     console.log(idade);
@@ -18,21 +30,25 @@ loginForm.addEventListener("submit", evento => {
     console.log(comprimento);
     let cor_primaria = document.getElementById("cor-primaria").value;
     console.log(cor_primaria);
-    let nome = document.getElementById("nome").value;
-    console.log(nome);
+    let nome_pet = document.getElementById("nome-pet").value;
+    console.log(nome_pet);
     let comportamento = document.getElementById("comportamento").value;
     console.log(comportamento);
 
     let data = {
         dono: {
-
+            email: email,
+            cpf: cpf,
+            tel: tel,
+            endereco: endereco,
+            nome: nome,
         },
         pet: {
             idade: idade,
             peso: peso,
             comprimento: comprimento,
             cor_primaria: cor_primaria,
-            nome: nome,
+            nome_pet: nome_pet,
             comportamento: comportamento,
         }
 
