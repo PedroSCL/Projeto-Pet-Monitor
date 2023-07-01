7f8e4262152b7a93640d40db5d3618338591bb22
Gustavo-CS <gustavocarvalho9340@gmail.com>
2023-07-01 10:58:49 -0300

    subindo as mudanças

--- firebase.js
+++ firebase.js
@@ -1,3 +1,4 @@
+// export default dataa;
 
 // configuração do firebase
 const firebaseConfig = {
@@ -28,18 +29,93 @@ function cadastrar() {
             alert("falhar ao cadastrar");
         });
 }
+// email = document.getElementById("email").value;
+// senha = document.getElementById("senha").value;
 
-function logar() {
+async function logar() {
     var auth = null;
     // função de cadastro do firebase com email e senha 
-    firebase.auth().signInWithEmailAndPassword(document.getElementById("email").value, document.getElementById("senha").value)
+
+    url = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDwmViBzQcjkwQ8a9ptPDAMR8S_h6iCj2c";
+    // url = "https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyDwmViBzQcjkwQ8a9ptPDAMR8S_h6iCj2c"
+
+    // response = await JSON.stringify({
+    //     'email': document.getElementById("email").value,
+    //     'password': document.getElementById("senha").value,
+    //     'returnSecureToken': true,
+    // });
+    //console.log(response);
+    //console.log(body);
+    response = new Request(url, {
+        method: 'POST',
+        body: JSON.stringify({
+            'email': document.getElementById("email").value,
+            'password': document.getElementById("senha").value,
+            'returnSecureToken': true,
+        }
+        ),
+
+        // body: data,
+    });
+    var dataa;
+
+    await fetch(response)
+        .then(res => res.json()).then(data => dataa = data
+        );
+    console.log('antes');
+    console.log(dataa);
+    console.log('depois');
+    console.log(dataa['localId']);
+    //document.getElementById("userId").innerHTML = 'test';
+
+
+
+
+    //dataa = JSON.parse(response);
+    //body = JSON.parse(response);
+    //console.log(body['idToken']);
+    //console.log(response.body['localId']);
+
+    // let data = {
+    //     // name: "cu",
+    //     email: {
+    //         email: 'email',
+    //         cpf: 'cpf',
+    //     }
+    // };
+
+    // const url2 = 'https://projeto-2-teste-c9255-default-rtdb.firebaseio.com/teste/.json';
+
+    // request = new Request(url2, {
+    //     method: 'POST',
+    //     // method: 'PATCH',
+    //     body: JSON.stringify(data),
+    //     // body: data,
+    //     headers: new Headers(),
+
+    // });
+    // await fetch(request)
+    //     .then(res => res.json()).then(data => console.log(data)
+    //     )
+
+
+    await firebase.auth().signInWithEmailAndPassword(document.getElementById("email").value, document.getElementById("senha").value)
         .then(function (user) {
-            alert("seus dados foi cadastrado com sucesso");
+            //alert("seus dados foi cadastrado com sucesso");
             auth = user;
+            //console.log(user.body)
+            // console.log(user.Token)
+            // console.log(user.token)
+            // console.log(user.localId)
             //para atualizar o navegador
             document.getElementById('email').value = ''
             document.getElementById('senha').value = ''
         }).catch(function (error) {
-            alert("falhar ao cadastrar");
+            //alert("falhar ao cadastrar");
         });
+    //window.location.href = "pag-cadas-usuario/index.html";
+    window.location.href = "variaveis.html";
 }
+//export default dataa;
+//export let myVariable = dataa;
+// export { dataa as default };
--- pet-cadastro/js/bd.js
+++ pet-cadastro/js/bd.js
@@ -1,3 +1,5 @@
+//import {dataa} from '../../firebase.js';
+const { dataa } = require('../../firebase.js');
 
 let loginForm = document.getElementById('loginForm');
 
@@ -7,9 +9,10 @@ loginForm.addEventListener("submit", evento => {
     // const data = Object.fromEntries(loginForm);
 
     //console.log(data);
-    const url = 'https://projeto-2-teste-c9255-default-rtdb.firebaseio.com/.json';
+    // const url = 'https://projeto-2-teste-c9255-default-rtdb.firebaseio.com/.json?auth=KBEH3eEdZNflAwEKNvftlyoDuDh2';
+    //var emailbd = email;
 
-    
+
 
     let email = document.getElementById("email").value;
     console.log(email);
@@ -21,46 +24,88 @@ loginForm.addEventListener("submit", evento => {
     console.log(endereco);
     let nome = document.getElementById("nome").value;
     console.log(nome);
+    const url = 'https://projeto-2-teste-c9255-default-rtdb.firebaseio.com/teste/.json';
 
-    let idade = document.getElementById("idade").value;
-    console.log(idade);
-    let peso = document.getElementById("peso").value;
-    console.log(peso);
-    let comprimento = document.getElementById("comprimento").value;
-    console.log(comprimento);
-    let cor_primaria = document.getElementById("cor-primaria").value;
-    console.log(cor_primaria);
-    let nome_pet = document.getElementById("nome-pet").value;
-    console.log(nome_pet);
-    let comportamento = document.getElementById("comportamento").value;
-    console.log(comportamento);
+
+    // let idade = document.getElementById("idade").value;
+    // console.log(idade);
+    // let peso = document.getElementById("peso").value;
+    // console.log(peso);
+    // let comprimento = document.getElementById("comprimento").value;
+    // console.log(comprimento);
+    // let cor_primaria = document.getElementById("cor-primaria").value;
+    // console.log(cor_primaria);
+    // let nome_pet = document.getElementById("nome-pet").value;
+    // console.log(nome_pet);
+    // let comportamento = document.getElementById("comportamento").value;
+    // console.log(comportamento);
 
     let data = {
-        dono: {
+        // name: "cu",
+        email: {
             email: email,
             cpf: cpf,
             tel: tel,
             endereco: endereco,
             nome: nome,
         },
-        pet: {
-            idade: idade,
-            peso: peso,
-            comprimento: comprimento,
-            cor_primaria: cor_primaria,
-            nome_pet: nome_pet,
-            comportamento: comportamento,
-        }
+        // dono: {
+        //     email: 'email',
+        //     cpf: 'cpf',
+        //     tel: 'tel',
+        //     endereco: 'endereco',
+        //     nome: 'nome',
+        // },
+        // pet: {
+        //     idade: idade,
+        //     peso: peso,
+        //     comprimento: comprimento,
+        //     cor_primaria: cor_primaria,
+        //     nome_pet: nome_pet,
+        //     comportamento: comportamento,
+        // }
 
-    }
+    };
 
-    var request = new Request(url, {
+    // var obj = {
+    //     dono: {
+    //         email: email,
+    //         cpf: cpf,
+    //         tel: tel,
+    //         endereco: endereco,
+    //         nome: nome,
+    //     },
+    //     pet: {
+    //         idade: idade,
+    //         peso: peso,
+    //         comprimento: comprimento,
+    //         cor_primaria: cor_primaria,
+    //         nome_pet: nome_pet,
+    //         comportamento: comportamento,
+    //     }
+    // };
+
+
+    request = new Request(url, {
         method: 'POST',
+        // method: 'PATCH',
         body: JSON.stringify(data),
-        headers: new Headers()
+        // body: data,
+        headers: new Headers(),
+
     });
+    // console.log(request);
+    // request.body.locked = false;
+    // resposta = JSON.parse(request.body);
+    // console.log(resposta);
+
+    // var request = new XMLHttpRequest();
+    // request.open("POST", url, true, 'teste', 'queisso');
+    // request.setRequestHeader('Content-Type', 'application/json');
+    // request.send(JSON.stringify(data));
 
     fetch(request)
         .then(res => res.json()).then(data => console.log(data)
         )
+    console.log(dataa['localId']);
 });
